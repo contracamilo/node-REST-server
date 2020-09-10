0755f9e3774143de95565788e93352f1f3d8a906
camilo.rivera <camilo.rivera@globant.com>
2020-09-09 23:25:52 -0500

    REST server beta

--- server/server.js
+++ server/server.js
@@ -1,43 +1,53 @@
-const express = require ('express');
-const app = express ();
-const bodyParser = require ('body-parser');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+require("./config/config");
+
+const {PORT} = process.env;
 
 /* 
 middlewares
     parse application/x-www-form-urlencoded
     parse application/json
 */
-app.use (bodyParser.urlencoded ({extended: false}));
-app.use (bodyParser.json ());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //read
-app.get ('/users', function (req, res) {
-  res.json ('Hello World');
+app.get("/users", function (req, res) {
+	res.json("Hello World");
 });
 
 //create
-app.post ('/users', function (req, res) {
-  let {body} = req;
+app.post("/users", function (req, res) {
+	let {body} = req;
 
-  res.json ({
-    user: body
-  });
+	if (!body.name) {
+		res.status(400).json({
+			ok: false,
+			message: "name not found",
+		});
+	} else {
+		res.json({
+			user: body,
+		});
+	}
 });
 
 //update
-app.put ('/users/:id', function (req, res) {
-  let {params} = req;
+app.put("/users/:id", function (req, res) {
+	let {params} = req;
 
-  res.json ({
-    id: params.id,
-  });
+	res.json({
+		id: params.id,
+	});
 });
 
 //delete
-app.delete ('/users', function (req, res) {
-  res.json ('Hello World');
+app.delete("/users", function (req, res) {
+	res.json("Hello World");
 });
 
-app.listen (3000, () => {
-  console.log ('listen on port: 3000');
+app.listen(PORT, () => {
+	console.log(`listen on port: ${PORT}`);
 });
